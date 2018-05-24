83ca408d6944b36b31ba9d66c8ca93d82f56ceba
marklippert <lippert@gmail.com>
2018-05-24 14:26:20 -0500

    Fix Instagram feed

--- footer.php
+++ footer.php
@@ -15,21 +15,37 @@
         </div>
       </div>
     </div>
-
-    <div class="social-tiles cf">
+    
+    <script type="text/javascript">
+      jQuery(document).ready(function(){
+        var jqxhr = jQuery.ajax("https://apinsta.herokuapp.com/u/harkenblockhead").done(function() {
+        }).fail(function() {
+        }).always(function(data) {
+          items = data.graphql.user.edge_owner_to_timeline_media.edges;
+          jQuery.each(items, function(n, item) {
+            if((n+1) <= 10) {
+              var data_links = '<a href="https://www.instagram.com/p/'+item.node.shortcode+'" style="background-image: url('+item.node.thumbnail_src+');"" target="new"></a>';
+              jQuery("#instafeed").append(data_links);
+            }
+          });
+        });
+      });
+    </script>
+    <div id="instafeed"></div>
+    <!-- <div class="social-tiles cf"> -->
       <?php
-      $count = 1;
-      $json = json_decode(file_get_contents('https://apinsta.herokuapp.com/u/harkenblockhead'));
-      foreach ($json->graphql->user->edge_owner_to_timeline_media->edges as $key => $value) {
-        if ($count <= 10) {
-          echo '<a href="'.'https://www.instagram.com/p/'.$value->node->shortcode.'" style="background-image: url('.$value->node->thumbnail_src.');" class="social instagram"></a>';
-        } else {
-          break;
-        }
-        $count++;
-      }
+      // $count = 1;
+      // $json = json_decode(file_get_contents('https://apinsta.herokuapp.com/u/harkenblockhead'));
+      // foreach ($json->graphql->user->edge_owner_to_timeline_media->edges as $key => $value) {
+      //   if ($count <= 10) {
+      //     echo '<a href="'.'https://www.instagram.com/p/'.$value->node->shortcode.'" style="background-image: url('.$value->node->thumbnail_src.');" class="social instagram"></a>';
+      //   } else {
+      //     break;
+      //   }
+      //   $count++;
+      // }
       ?>
-    </div>
+    <!-- </div> -->
     <?php } ?>
 
     <div class="footer-contact">
