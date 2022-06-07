47772706e38059fc6211661ef86447f8c4be9700
foresitegroup <mark@foresitegrp.com>
2022-06-07 13:49:16 -0500

    Add new analytics code

--- header.php
+++ header.php
@@ -76,15 +76,24 @@ if (isset($PageTitleLangPlus)) $PageTitle .= $PageTitleLangPlus;
         $(".swipebox").swipebox({hideBarsDelay : 0});
       });
     </script>
-
+    
+    <!-- Google Analytics [This will stop working 7/1/23] -->
     <script>
       (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
       (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
       m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
       })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
       ga('create', 'UA-90627023-1', 'auto');
       ga('send', 'pageview');
+    </script>
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-PSSVQ9Z93P"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-PSSVQ9Z93P');
     </script>
   </head>
   <body class="<?php echo $language; if (isset($BodyClass)) echo " " . $BodyClass; ?>">
