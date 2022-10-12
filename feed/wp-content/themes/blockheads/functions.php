3c1dae826f4e544075931c4b11cc9297147a63c4
Mark Lippert <mark@foresitegrp.com>
2022-10-12 11:47:03 -0500

    Remove plugin update notification email

--- feed/wp-content/themes/blockheads/functions.php
+++ feed/wp-content/themes/blockheads/functions.php
@@ -1,4 +1,9 @@
 <?php
+// I don't need to be notified about every automatic update
+add_filter( 'auto_plugin_update_send_email', '__return_false' );
+add_filter( 'auto_theme_update_send_email', '__return_false' );
+
+
 // Set length of blog index except
 function wpdocs_custom_excerpt_length( $length ) {
   return 18;
